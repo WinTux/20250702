cbbd21c7b4c2b9cecf883cf0e82a2098121acc8a
WinTux <rusok.software@gmail.com>
2025-07-10 21:23:26 -0400

    Multimedia

--- ElementosGraficosMAUI/Pages/Ejemplo5page.xaml.cs
+++ ElementosGraficosMAUI/Pages/Ejemplo5page.xaml.cs
@@ -6,4 +6,20 @@ public partial class Ejemplo5page : ContentPage
 	{
 		InitializeComponent();
 	}
+	public void OnPlayPauseClicked(object sender, EventArgs e) {
+		if(video4.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+			video4.Pause();
+		else
+			video4.Play();
+
+    }
+	public void OnReducirVolClic(object sender, EventArgs e) { 
+		if(video4.Volume > 0)
+			video4.Volume -= 0.1;
+    }
+    public void OnAumentarVolClic(object sender, EventArgs e)
+    {
+        if (video4.Volume < 1)
+            video4.Volume += 0.1;
+    }
 }
